4aba3528915486a8b84ae8e23bad42f4d5ec606e
OlehLinskyy <118620479+OlehLinskyy@users.noreply.github.com>
2023-04-09 23:43:55 +0000

    Deploying to gh-pages from @ OlehLinskyy/goit-js-hw-09@cd7c558f75977d49a822e0b39aac7593c8ab7ea4 🚀

--- 01-color-switcher.80e64821.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.querySelector("body");t.addEventListener("click",(function(){timerId=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(timerId),t.disabled=!1,e.disabled=!0}));
-//# sourceMappingURL=01-color-switcher.80e64821.js.map
