b3e8bb9ebfae6b28b3cbc4a490d6a24edad9eb67
Oleh Linskyi <118620479+OlehLinskyy@users.noreply.github.com>
2023-04-10 03:02:38 +0300

    update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,6 +1,7 @@
 import Notiflix from 'notiflix';
 
 const formEl = document.querySelector('form');
+const buttonStartEl = formEl.querySelector('button')
 const inputDelayEl = formEl.delay;
 const inputStepEl = formEl.step;
 const inputAmountEl = formEl.amount;
@@ -9,7 +10,8 @@ formEl.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(evt) {
   evt.preventDefault();
-
+  buttonStartEl.disabled = true;
+  
   const delay = Number(inputDelayEl.value);
   const step = Number(inputStepEl.value);
   const amount = Number(inputAmountEl.value);
